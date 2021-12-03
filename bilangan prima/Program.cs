52129ef413e5b4b719bcf8796306f6273b884df9
Michelle Angela <michelleaw2004@outlook.com>
2021-12-03 10:24:20 +0700

    Add project files.

---
+++ bilangan prima/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace bilangan_prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mengecek bilangan prima, masukkan angka: ");
+            int angka = Convert.ToInt32(Console.ReadLine());
+            int hitung = 0;
+            for (int i = 1; i<=angka;i++)
+            {
+                if (angka % i == 0)
+                {
+                    hitung = hitung +1 ;
+                }
+               
+            }
+            if (hitung == 2)
+            {
+                Console.Write("Angka bilangan prima");
+            }
+            else
+            {
+                Console.Write("Angka bukan bilangan prima");
+            }
+        }
+    }
+}
